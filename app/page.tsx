2b975777e5e96c5dbd429fc823dd33101ea29bf8
Alex Monro <alexmonro546@gmail.com>
2026-05-21 15:41:03 -0700

    add upload proccessing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import UploadZone from "@/components/UploadZone";
+import { useState } from "react";
 
 export default function Home() {
+  const [audioFile, setAudioFile] = useState<File | null>(null);
+
   return (
     <div className="flex flex-col items-center justify-center gap-12 pt-24 ">
       <h1 className="font-black italic text-5xl tracking-tightest">
@@ -10,7 +15,7 @@ export default function Home() {
         Upload an audio track and get vocals, drums, bass, and melody separated.
       </p>
       <div className="mt-12 w-full">
-        <UploadZone />
+        <UploadZone setAudioFile={setAudioFile} />
       </div>
     </div>
   );
--- components/UploadZone.tsx
+++ components/UploadZone.tsx
@@ -1,9 +1,33 @@
 import { Download } from "lucide-react";
+import { useRef } from "react";
 
-const UploadZone = () => {
+const UploadZone = ({
+  setAudioFile,
+}: {
+  setAudioFile: (audioFile: File) => void;
+  // Take this funtion as a propt. say what type of file is expected and what it should return
+}) => {
+  const input = useRef<HTMLInputElement>(null);
+
+  const handleUploadClick = () => {
+    input.current?.click();
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center border-2 border-dashed border-black w-full max-w-4xl mx-auto py-30 rounded-4xl">
-      <input type="file" accept="audio/*" className="hidden" id="file-upload" />
+    // This is the upload zone. I used a ref to make sure that when the user clicks on the div, it actually triggers the input properties
+    <div
+      onClick={handleUploadClick}
+      //over ride the default browser behavior
+      onDragOver={(e) => e.preventDefault()}
+      className="flex flex-col items-center justify-center border-2 border-dashed border-black w-full max-w-4xl mx-auto py-30 rounded-4xl"
+    >
+      <input
+        type="file"
+        accept="audio/*"
+        className="hidden"
+        id="file-upload"
+        ref={input}
+      />
       <Download className="w-8 h-8 " />
       <p className="mt-4 text-lg font-bold">Drop your audio file here.</p>
       <p className="mt-4 text-md text-gray-700">
